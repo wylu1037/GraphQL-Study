0802d6c29b7501955982d802a8548215fc31e3bd
卢文扬 <2052987369@qq.com>
2022-09-18 15:33:03 +0800

    chore:依赖版本修改

--- src/main/java/cn/edu/hust/graphqlstudy/config/MybatisPlusConfig.java
+++ src/main/java/cn/edu/hust/graphqlstudy/config/MybatisPlusConfig.java
@@ -1,10 +1,11 @@
 package cn.edu.hust.graphqlstudy.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.sql.DataSource;
-
+import com.baomidou.mybatisplus.core.MybatisConfiguration;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.InnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.type.JdbcType;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -12,12 +13,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
-import com.baomidou.mybatisplus.core.MybatisConfiguration;
-import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.inner.InnerInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * MybatisPlus配置
@@ -42,14 +40,13 @@ public class MybatisPlusConfig {
         sessionFactory.setDataSource(dataSource);
 
         MybatisConfiguration configuration = new MybatisConfiguration();
-        // configuration.setDefaultScriptingLanguage(MybatisXMLLanguageDriver.class);
         configuration.setJdbcTypeForNull(JdbcType.NULL);
         configuration.setCacheEnabled(true);
         // 解决属性大写变小写加下划线的问题
         configuration.setMapUnderscoreToCamelCase(false);
         configuration.setCallSettersOnNulls(true);
         // 正式环境需要屏蔽：MybatisPlus执行SQL时，将其打印出来
-        // configuration.setLogImpl(org.apache.ibatis.logging.stdout.StdOutImpl.class);
+        configuration.setLogImpl(org.apache.ibatis.logging.stdout.StdOutImpl.class);
         sessionFactory.setConfiguration(configuration);
 
         PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
--- src/main/java/cn/edu/hust/graphqlstudy/fetcher/ConsortiumChainFetcher.java
+++ src/main/java/cn/edu/hust/graphqlstudy/fetcher/ConsortiumChainFetcher.java
@@ -1,22 +1,15 @@
 package cn.edu.hust.graphqlstudy.fetcher;
 
-import java.util.List;
-
-import com.baomidou.mybatisplus.core.conditions.Wrapper;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.toolkit.Wrappers;
-import com.netflix.graphql.dgs.DgsComponent;
-import com.netflix.graphql.dgs.DgsData;
-import com.netflix.graphql.dgs.DgsDataFetchingEnvironment;
-import com.netflix.graphql.dgs.DgsQuery;
-import com.netflix.graphql.dgs.InputArgument;
-
 import cn.edu.hust.graphqlstudy.bean.entity.ChainNode;
 import cn.edu.hust.graphqlstudy.bean.entity.ConsortiumChain;
 import cn.edu.hust.graphqlstudy.bean.entity.User;
 import cn.edu.hust.graphqlstudy.mapper.ChainNodeMapper;
 import cn.edu.hust.graphqlstudy.mapper.ConsortiumChainMapper;
 import cn.edu.hust.graphqlstudy.mapper.UserMapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.netflix.graphql.dgs.*;
+
+import java.util.List;
 
 @DgsComponent
 public class ConsortiumChainFetcher {
@@ -25,9 +18,9 @@ public class ConsortiumChainFetcher {
     private final ChainNodeMapper chainNodeMapper;
     private final UserMapper userMapper;
 
-    public ConsortiumChainFetcher(ConsortiumChainMapper consortiumChainMapper, 
-    ChainNodeMapper chainNodeMapper,
-    UserMapper userMapper) {
+    public ConsortiumChainFetcher(ConsortiumChainMapper consortiumChainMapper,
+                                  ChainNodeMapper chainNodeMapper,
+                                  UserMapper userMapper) {
         this.consortiumChainMapper = consortiumChainMapper;
         this.chainNodeMapper = chainNodeMapper;
         this.userMapper = userMapper;
@@ -48,7 +41,7 @@ public class ConsortiumChainFetcher {
     public List<ChainNode> findNodeListByChainId(DgsDataFetchingEnvironment dfe) {
         ConsortiumChain chain = dfe.getSource();
         return chainNodeMapper.selectList(Wrappers.<ChainNode>lambdaQuery()
-            .eq(ChainNode::getChainId, chain.getId())
+                .eq(ChainNode::getChainId, chain.getId())
         );
     }
 }
